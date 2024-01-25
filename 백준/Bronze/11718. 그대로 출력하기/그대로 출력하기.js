c7f13927d53d26e06684e1fe79231158406ec57e
Dongho Lee <67420465+rustandbone@users.noreply.github.com>
2024-01-26 00:59:05 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 128 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,9 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+console.log(input.join('\n'));
