7c005db6f21d459ab8fef853a1a8af43a1e89690
rustandbone <dlehdgh14@naver.com>
2024-01-26 00:37:18 +0900

    0126 금 풀이

---
+++ BAEKJOON/27866.js
@@ -0,0 +1,9 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+console.log(input[0][+input[1] - 1]);
