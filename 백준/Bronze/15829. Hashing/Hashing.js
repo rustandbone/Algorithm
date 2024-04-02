00a1ff9854ee4d1b12fde25970b8c5d5fc29da97
Dongho Lee <67420465+rustandbone@users.noreply.github.com>
2024-04-02 15:33:56 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 9340 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -12,9 +12,18 @@ const alphabet = Array(26)
   .fill()
   .map((v, i) => String.fromCharCode(i + 97));
 
-const answer = input[0].split('').reduce((acc, cur, i) => {
-  const cal = (alphabet.indexOf(cur) + 1) * 31 ** i;
-  return acc + cal;
-}, 0);
+let answer = 0n;
+let r = 1n;
 
-console.log(answer);
+input[0].split('').forEach((v) => {
+  let cal = BigInt(alphabet.indexOf(v) + 1) * r;
+  cal %= 1234567891n;
+
+  answer += cal;
+  answer %= 1234567891n;
+
+  r *= 31n;
+  r %= 1234567891n;
+});
+
+console.log(answer.toString());
