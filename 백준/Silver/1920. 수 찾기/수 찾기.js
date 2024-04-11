d93115e3383d7793de986966b1196205f3de23c6
Dongho Lee <67420465+rustandbone@users.noreply.github.com>
2024-04-11 17:04:07 +0900

    [Silver IV] Title: 수 찾기, Time: 296 ms, Memory: 38588 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -2,14 +2,10 @@ const filePath = process.platform === 'linux' ? 0 : './example.txt';
 
 const input = require('fs').readFileSync(filePath, 'utf-8').trim().split('\n');
 
-const n = {};
+const n = new Set(input[1].trim().split(' '));
 
-input[1].split(' ').forEach((number) => (n[+number] = 1));
-
-input[3].split(' ').forEach((number) => {
-  if (n[number]) {
-    console.log(1);
-  } else {
-    console.log(0);
-  }
+const check = input[3].split(' ').map((number) => {
+  return n.has(number) ? 1 : 0;
 });
+
+console.log(check.join('\n'));
