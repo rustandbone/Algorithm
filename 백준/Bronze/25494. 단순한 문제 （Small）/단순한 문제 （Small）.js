3ebbc3c3c7527f5f8891c9969bb14527be7acd23
Dongho Lee <67420465+rustandbone@users.noreply.github.com>
2024-04-29 12:12:16 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 136 ms, Memory: 9360 KB -BaekjoonHub

--- 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.js
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.js
@@ -5,18 +5,8 @@ const input = require('fs').readFileSync(filePath, 'utf-8').trim().split('\n');
 input.shift();
 
 const answer = input.map((v) => {
-  let count = 0;
   const [a, b, c] = v.split(' ').map(Number);
-
-  for (let i = 1; i <= a; i++) {
-    for (let j = 1; j <= b; j++) {
-      for (let k = 1; k <= c; k++) {
-        if (i % j === j % k && k % i === j % k) count++;
-      }
-    }
-  }
-
-  return count;
+  return Math.min(a, b, c);
 });
 
 console.log(answer.join('\n'));
