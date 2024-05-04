d7189cf5986cb5dcae2faf1e672ae2f6cdbb8a0f
Dongho Lee <67420465+rustandbone@users.noreply.github.com>
2024-05-04 13:44:35 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 256 ms, Memory: 12280 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.js
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.js
@@ -1,31 +1,32 @@
-const filePath = process.platform === 'linux' ? 0 : './example.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
 
 const input = require('fs').readFileSync(filePath, 'utf-8').trim().split('\n');
 
 const [songsNum] = input[0].split(' ').map(Number);
 
-const songs = {};
+const codeMap = new Map();
 const songsArr = input.slice(1, songsNum + 1);
 const questions = input.slice(songsNum + 1);
 
 songsArr.forEach((song) => {
   const [, title, ...code] = song.trim().split(' ');
-  songs[title] = code.slice(0, 3).join(' ');
+  const codeKey = code.slice(0, 3).join(' ');
+  if (!codeMap.has(codeKey)) {
+    codeMap.set(codeKey, [title]);
+  } else {
+    codeMap.get(codeKey).push(title);
+  }
 });
 
 questions.forEach((question) => {
-  const titleList = [];
   question = question.trim();
+  const titleList = codeMap.get(question) || [];
 
-  Object.entries(songs).forEach(([title, code]) => {
-    if (code.includes(question)) {
-      titleList.push(title);
-    }
-  });
-
-  titleList.length === 0
-    ? console.log('!')
-    : titleList.length > 1
-    ? console.log('?')
-    : console.log(titleList[0]);
+  if (titleList.length === 0) {
+    console.log('!');
+  } else if (titleList.length > 1) {
+    console.log('?');
+  } else {
+    console.log(titleList[0]);
+  }
 });
